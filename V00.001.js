73c673ddd94719a321210cf7fc16ced3bcfc7b9a
saidzac2000 <147353811+saidzac2000@users.noreply.github.com>
2024-08-22 16:45:51 -0500

    Update V00.001.js

--- V00.001.js
+++ V00.001.js
@@ -1,33 +1,4 @@
-// Variable
-// Functions
-export function Write({
-    element, 
-    write, 
-    size, 
-    color, 
-    align, 
-    font, 
-    display,
-    parentnode
-}) {
-  // V0.01
-  var text = document.createElement(element);
-  text.textContent = write;
-  text.classList.add("text");
-  text.style.fontSize = size + 'px';
-  text.style.color = color;
-  text.style.textAlign = align;
-  text.style.fontFamily = font;
-  text.style.display = display; 
-  parentnode.appendChild(text);
+function start(){
+    alert("hello");
 }
-
-Write({
-    element: "p",
-    write: "hello",
-    size: 100,
-    color: "red",
-    align: "center",
-    font: "Cursive",
-    display: "block"
-});
+export default start()
